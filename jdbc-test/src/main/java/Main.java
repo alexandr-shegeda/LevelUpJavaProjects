bab94b2907fa40316c99356d3fb23a835ba36b09
alexandr-shegeda <alexandr.shegeda@gmail.com>
2017-03-21 21:01:02 +0200

    added a lot of changes

--- jdbc-test/src/main/java/Main.java
+++ jdbc-test/src/main/java/Main.java
@@ -1,5 +1,7 @@
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by java on 17.03.2017.
@@ -8,19 +10,43 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IllegalAccessException, InstantiationException {
-        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        Class.forName("com.mysql.jdbc.Driver");
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/forum", "root", "qwerty");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/forum",
+                "root", "qwerty");
+
+        PreparedStatement preparedStatement = connection.prepareStatement
+                ("INSERT INTO USERS (USERNAME, EMAIL, PASSWORD) VALUES(?,?,?)");
+
+        User user = new User("Bob Martin", "bobm@yopmail.com", "1234");
+
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getEmail());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.execute();
+
 
         Statement statement = connection.createStatement();
 
         ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
 
+        List<User> userList = new ArrayList<User>();
         while (resultSet.next()) {
+            long id = resultSet.getLong("ID");
+            String username = resultSet.getString("USERNAME");
+            String email = resultSet.getString("EMAIL");
+            String pass = resultSet.getString("PASSWORD");
+
+            userList.add(new User(id, username, email, pass));
+
             System.out.println(resultSet.getLong("ID")
                     + " " + resultSet.getString("USERNAME")
                     + " " + resultSet.getString("EMAIL"));
         }
+
+
+        System.out.println(userList);
 
         connection.close();
     }
