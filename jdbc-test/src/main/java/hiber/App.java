249b4b3a5e50e9a8753c7364c5695a0f3f9e7929
alexandr-shegeda <alexandr.shegeda@gmail.com>
2017-03-31 20:59:19 +0300

    added a lot of changes

--- jdbc-test/src/main/java/hiber/App.java
+++ jdbc-test/src/main/java/hiber/App.java
@@ -2,6 +2,10 @@ package hiber;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 import static hiber.HibernateUtil.getSessionFactory;
 
@@ -12,9 +16,28 @@ public class App {
 
     public static void main(String[] args) {
         SessionFactory sessionFactory = getSessionFactory();
-
-//        Session session = sessionFactory.openSession();
 //
-//        session.createQuery("", User.class);
+//        Employee employee = new Employee();
+
+        try (Session session = sessionFactory.openSession();) {
+
+            Employee employee = new Employee("Andrey", "Ivanov", "Robertovich", 55000);
+
+            Transaction transaction = session.getTransaction();
+
+            transaction.begin();
+            session.save(employee);
+            transaction.commit();
+
+            Query<Employee> query = session.createQuery("from Employee", Employee.class);
+            List<Employee> list = query.list();
+
+            for (Employee e : list) {
+                System.out.println(e);
+            }
+
+        } finally {
+            System.out.println("Good bye!");
+        }
     }
 }
