a842a5e64bf49dd881003466c4101f3a0e3159aa
alexandr-shegeda <alexandr.shegeda@gmail.com>
2017-04-21 21:02:14 +0300

    added producer

---
+++ lesson43/src/email/Main.java
@@ -0,0 +1,32 @@
+package email;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+import static email.EmailSender.INSTANCE;
+
+/**
+ * Created by java on 21.04.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+//        INSTANCE.sendEmail("petrosyan@yopmail.com", "Hello mr Petrosyan", "Very argent");
+//        emailSender.sendEmail();
+
+        ArrayBlockingQueue<EmailMessage> queue = new ArrayBlockingQueue<>(10);
+        EmailProducer producer = new EmailProducer(queue);
+
+        EmailConsumer consumer1 = new EmailConsumer(queue);
+        EmailConsumer consumer2 = new EmailConsumer(queue);
+        EmailConsumer consumer3 = new EmailConsumer(queue);
+
+        producer.start();
+
+        consumer1.start();
+        consumer2.start();
+        consumer3.start();
+
+
+        Thread.sleep(10000);
+    }
+}
